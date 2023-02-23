538a6ad30983dc5f6293332acae5c00f5d695bd0
harshsareen03 <harshsareen03@gmail.com>
2023-02-23 23:04:28 +0530

    version1

---
+++ session_16/syntax.js
@@ -0,0 +1,45 @@
+console.log('hi')
+
+// js values 
+
+
+// var x=30
+// let y=10
+// const k=20
+
+
+// 1)fixed value value-they are literals
+// 2)variable values-variables
+
+// numbers
+
+var x=20.50
+console.log(x)
+
+// strings
+var y='hello'
+"hello"
+
+//what are variables?
+// -variables are used to store data values
+// we can define variables in three variations.
+//- 1)var , 2)let , 3)const
+
+// var is non-blocking variables
+
+var a = 5
+console.log(a)
+var a = 10
+console.log(a)
+var a = 30
+console.log(a)
+
+// let and const are blocking variables
+
+let b=10
+
+console.log(b)
+let b=20
+
+const c=20
+
